b517593a9de524df1e31086340f8f5d555dbf64d
Satbek Abdyldayev <satbekmyrza@gmail.com>
2023-02-11 06:37:27 +0000

    add Dog class

---
+++ 12pm/problem-A/Dog.java
@@ -0,0 +1,17 @@
+public class Dog extends Mammal {
+    public Dog(String n) {
+        super(n);
+    }
+
+    public void greets() {
+        System.out.println("Woof");
+    }
+
+    public void greets(Dog another) {
+        System.out.println("Woooof");
+    }
+
+    public String toString() {
+        return String.format("Dog[%s]", super.toString());
+    }
+}
