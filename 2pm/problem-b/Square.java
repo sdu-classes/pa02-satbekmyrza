1ee4a362e18ade2935ed0bc2a7e23c506eceb650
Satbek Abdyldayev <satbekmyrza@gmail.com>
2023-02-11 09:53:22 +0000

    added Square class

---
+++ 2pm/problem-b/Square.java
@@ -0,0 +1,32 @@
+public class Square extends Rectangle {
+    public Square() {
+
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(side, side, color, filled);
+    }
+
+    public double getSide() {
+        return this.getWidth();
+    }
+
+    public void setSide(double side) {
+        super.setLength(side);
+        super.setWidth(side);
+    }
+
+    @Override
+    public void setWidth(double side) {
+        this.setWidth(side);
+    }
+
+    @Override
+    public void setLength(double side) {
+        this.setWidth(side);
+    }
+}
