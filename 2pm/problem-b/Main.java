7bb401dffa3e7c82b0ca8c98af25e58e20b42a7a
Satbek Abdyldayev <satbekmyrza@gmail.com>
2023-02-11 09:15:27 +0000

    added Shape, Main classes

---
+++ 2pm/problem-b/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Shape sh1 = new Shape();
+        Shape sh2 = new Shape("black", false);
+
+        System.out.println(sh1);
+        System.out.println(sh2);
+        
+    }
+}
