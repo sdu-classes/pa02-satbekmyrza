4f323f7a6066a887e0c8a7656b4f2ae5d80bdc82
Satbek Abdyldayev <satbekmyrza@gmail.com>
2023-02-11 07:20:52 +0000

    added Rectangle class

--- 12pm/problem-B/Main.java
+++ 12pm/problem-B/Main.java
@@ -14,12 +14,12 @@ public class Main {
         System.out.println(circle.isFilled()); 
         System.out.println(circle.getRadius()); 
  
-        // Rectangle rectangle = new Rectangle(3.8, 2.5, "green", false); 
-        // System.out.println(rectangle); 
-        // System.out.println(rectangle.getArea()); 
-        // System.out.println(rectangle.getPerimeter()); 
-        // System.out.println(rectangle.getColor()); 
-        // System.out.println(rectangle.getLength()); 
+        Rectangle rectangle = new Rectangle(3.8, 2.5, "green", false); 
+        System.out.println(rectangle); 
+        System.out.println(rectangle.getArea()); 
+        System.out.println(rectangle.getPerimeter()); 
+        System.out.println(rectangle.getColor()); 
+        System.out.println(rectangle.getLength()); 
  
  
         // Square square = new Square(6.6); 
